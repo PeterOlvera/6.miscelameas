ba4ead4a30b03ad6a57b5f4b296baa78a7e2aa97
Peter Olvera <p.i.t.e.r.olvera@gmail.com>
2018-02-06 17:20:53 +0100

    ngStyle y ngClass demo implementada

---
+++ src/app/components/ng-class/ng-class.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-class',
+  templateUrl: './ng-class.component.html',
+  styles: []
+})
+export class NgClassComponent implements OnInit {
+
+  tipoAlerta:String = "alert-info"
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
