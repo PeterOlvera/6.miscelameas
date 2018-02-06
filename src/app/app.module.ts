388bf594082e3936054428bcd1a2ac91abc67043
PeterOlvera <p.i.t.e.r.olvera@gmail.com>
2018-02-06 20:59:09 +0100

    boton guardar implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgClassComponent } from './components/ng-class/ng-class.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgClassComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/ng-class/ng-class.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-class',
+  templateUrl: './ng-class.component.html',
+  styles: []
+})
+export class NgClassComponent implements OnInit {
+
+
+  loading:boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ejecutar(){
+    this.loading=true; 
+    setTimeout( ()=> this.loading = false, 3000 );
+  }
+
+}
